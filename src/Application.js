2922574628ce01510b3db18a1d65509ca23624ab
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-05-02 15:48:10 +0100

    start clean

---
+++ src/Application.js
@@ -0,0 +1,13 @@
+import './Application.css';
+
+function Application() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Posts</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Application;
